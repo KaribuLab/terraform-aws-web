296c20f06ae810aa25520fe807110c96985b9ed5
Patricio Ascencio <patricio.ascencio@karibu.cl>
2026-03-03 13:57:15 -0300

    feat: Añade políticas de acceso para CloudFront en el bucket S3, permitiendo acciones de obtención y listado de objetos,  para que CloudFront retorne 404 si no encuentra un Key

--- main.tf
+++ main.tf
@@ -225,7 +225,7 @@ resource "aws_s3_bucket_policy" "s3_origin" {
     Version = "2012-10-17"
     Statement = [
       {
-        Sid = "${uuid()}"
+        Sid = "AllowCloudFrontServicePrincipalGetObject"
         Principal = {
           Service = "cloudfront.amazonaws.com"
         }
@@ -235,6 +235,24 @@ resource "aws_s3_bucket_policy" "s3_origin" {
         Effect = "Allow"
         Resource = [
           "arn:aws:s3:::${aws_s3_bucket.s3_origin[count.index].bucket}/*"
+        ],
+        Condition : {
+          StringEquals : {
+            "aws:SourceArn" : "${aws_cloudfront_distribution.distribution.arn}"
+          }
+        },
+      },
+      {
+        Sid = "AllowCloudFrontServicePrincipalListBucket"
+        Principal = {
+          Service = "cloudfront.amazonaws.com"
+        }
+        Action = [
+          "s3:ListBucket",
+        ]
+        Effect = "Allow"
+        Resource = [
+          "arn:aws:s3:::${aws_s3_bucket.s3_origin[count.index].bucket}"
         ],
         Condition : {
           StringEquals : {
